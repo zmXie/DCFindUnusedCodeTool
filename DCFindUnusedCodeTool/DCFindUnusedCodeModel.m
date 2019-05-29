3e9cb3263974713f73079af269f69dc65f8b9654
xiezhimin <xiezm@dachentech.com.cn>
2019-05-29 19:16:34 +0800

    提高匹配精度

--- DCFindUnusedCodeTool/DCFindUnusedCodeModel.m
+++ DCFindUnusedCodeTool/DCFindUnusedCodeModel.m
@@ -125,14 +125,13 @@
     //自身的文件名
     NSString* mFileName = dir.lastPathComponent.stringByDeletingPathExtension;
     NSString*pathExtension = dir.pathExtension;
-    //获取到所有类的集合
-    if ([pathExtension isEqualToString:@"h"] || [pathExtension isEqualToString:@"m"] || [pathExtension isEqualToString:@"mm"] || [pathExtension isEqualToString:@"xib"] || [pathExtension isEqualToString:@"storyboard"] || [pathExtension isEqualToString:@"pch"]) {
+    //正则匹配import的类、xib与storybord中引用的类、动态调用类
+    void (^addUsedClass)(NSString *,BOOL) = ^(NSString *regularStr,BOOL doubleCheck){
         //打开文件
         NSError *error;
         NSString* contentFile = [NSString stringWithContentsOfFile:dir encoding:NSUTF8StringEncoding error:&error];
         if (contentFile.length == 0) return;
-        //正则匹配import的类，以及xib与storybord中引用的类
-        NSString *regularStr = [NSString stringWithFormat:@"%@|%@|%@",@"#import.+\"",@"#import.+>",@"customClass=.+?\""];
+        contentFile = [self clearNotesWithContentFile:contentFile];
         NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:regularStr options:0 error:nil];
         NSArray* matches = [regex matchesInString:contentFile options:0 range:NSMakeRange(0, contentFile.length)];
         for (NSTextCheckingResult *result in matches) {
@@ -154,9 +153,56 @@
             if ([fileName isEqualToString:mFileName]) { //本文件自身
                 continue;
             }
-            [_usedClasses addObject:fileName];
+            //文件中使用了该类
+            if (doubleCheck && ![self checkClass:fileName inFile:contentFile]) {
+                continue;
+            }
+            [self->_usedClasses addObject:fileName];
         }
+    };
+    //类文件
+    if ([pathExtension isEqualToString:@"h"] || [pathExtension isEqualToString:@"m"] || [pathExtension isEqualToString:@"mm"] || [pathExtension isEqualToString:@"pch"]) {
+        //匹配import
+        NSString *importRegular = [NSString stringWithFormat:@"%@|%@",@"#import.+\"",@"#import.+>"];
+        addUsedClass(importRegular,NO);
+        //匹配动态调用
+        addUsedClass(@"NSStringFromClass\\(@.+?\\)",NO);
+    } else if ([pathExtension isEqualToString:@"xib"] || [pathExtension isEqualToString:@"storyboard"]) {
+        //IB文件
+        addUsedClass(@"customClass=.+?\"",NO);
     }
+}
+
+/**
+ 清理注释
+
+ @param contentFile 内容文件
+ @return 清理结果
+ */
+- (NSString *)clearNotesWithContentFile:(NSString *)contentFile
+{
+    //  \反斜杠在字符串中具有转义功能，\\双反斜杠表示前\对后\进行转义，即\字符串
+    NSString *regularStr = [NSString stringWithFormat:@"%@|%@",@"/\\*[\\s\\S]*?\\*/",@"//.*?\\n"];
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:regularStr options:0 error:nil];
+    contentFile = [regex stringByReplacingMatchesInString:contentFile options:0 range:NSMakeRange(0, contentFile.length) withTemplate:@""];
+    return contentFile;
+}
+
+/**
+ 检查文件中是否使用了该类，会有误伤
+
+ @param className 类
+ @param contentFile 文件
+ @return 是否使用
+ */
+- (BOOL)checkClass:(NSString *)className inFile:(NSString *)contentFile
+{
+    if ([className containsString:@"+"]) { //类目不做二次检查
+        return YES;
+    }
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:className options:0 error:nil];
+    NSArray* matches = [regex matchesInString:contentFile options:0 range:NSMakeRange(0, contentFile.length)];
+    return matches.count > 1;
 }
 
 /**
